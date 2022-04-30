a03f44201e4728a99f50fd523847bddd1d2d3dab
y4suyuki <y4suyuki@pm.me>
2022-04-30 14:18:05 +0900

    wip

--- src/background.ts
+++ src/background.ts
@@ -1,3 +1,16 @@
-const x = (y: number) => {
-  return y * 2
-}
+chrome.runtime.onInstalled.addListener(async ({ reason }) => {
+  console.log('reason:', reason)
+
+  if (reason === 'install') {
+    console.log('on install')
+
+    chrome.tabs.create({
+      url: 'onboarding.html'
+    })
+  } else if (reason === 'update') {
+    console.log('on update')
+    chrome.tabs.create({
+      url: 'onboarding.html'
+    })
+  }
+})
